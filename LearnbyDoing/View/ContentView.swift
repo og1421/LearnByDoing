e3f0fe26499a91815c9a4362c2386844083b49d7
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-02-23 21:12:53 -0300

    Creating the cards view

---
+++ LearnbyDoing/View/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LearnbyDoing
+//
+//  Created by Orlando Moraes Martins on 23/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    //MARK: - Properties
+    
+    //MARK: - Body
+    var body: some View {
+        CardView()
+    }
+}
+
+
+//MARK: - Preview
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
